cffcfbc562997b0db129d07de35e468b1168381b
Kahina <bndkahi73@gmail.com>
2025-01-29 17:32:49 +0100

    added booking

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -9,6 +9,7 @@ router.post('/', (req, res) => {
             trips.forEach(el => {
                 const newBooking = new Booking({
                     tripId: el.tripId,
+                    reservationDate : el.reservationDate,
                 })
                 newBooking.save().then();
             });
