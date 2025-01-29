420114dae71ff7cd43a237ee51a2e4b3d4e57c27
yirui Dupuy <yirui.hao@gmail.com>
2025-01-29 20:08:48 +0100

    fixed button book

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -9,7 +9,7 @@ router.post('/', (req, res) => {
             trips.forEach(el => {
                 const newBooking = new Booking({
                     tripId: el.tripId,
-                    reservationDate : el.reservationDate,
+                    reservationDate:new Date()
                 })
                 newBooking.save().then();
             });
